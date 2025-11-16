b310e2890eaa13647849c8fe33669dd673644aa6
Bnguyen8091 <nguyenbrian562@gmail.com>
2025-11-16 16:12:11 -0500

    Initial commit - Auto-generated LoopBack API

---
+++ src/models/book.model.ts
@@ -0,0 +1,38 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Book extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,   // let LoopBack auto-generate the id
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  author: string;
+
+  @property({
+    type: 'number',
+  })
+  year?: number;
+
+  constructor(data?: Partial<Book>) {
+    super(data);
+  }
+}
+
+export interface BookRelations {
+  // describe navigational properties here
+}
+
+export type BookWithRelations = Book & BookRelations;
